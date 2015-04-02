f306fec7f2036f36708705734ee82f8044122071
Anders E Bilgrau <anders.ellern.bilgrau@gmail.com>
2015-04-02 17:20:36 +0200

    Clean-up.

--- R/microarrayScale.R
+++ R/microarrayScale.R
@@ -2,41 +2,47 @@
 #' 
 #' Scale expression values prior to use of classification algorithms.
 #' 
-#' @param x matrix containing expression values. May also be of class ExpressionSet. 
-#' @param center How should the data be centered. Can be either "median", "mean", or "FALSE".
-#' @param scale How should the data be scaled. Can be either "sd" or "FALSE".
-#' @return Expression matrix consisting of genewise median centred and scaled genes.
-#' @details First each probe-set is median centred. Next, each probe-set is scaled to have unit-variance.
-#' @references Reference to the hemaClass.com paper
-#' @author Steffen Falgreen <sfl (at) rn.dk> \cr Anders Ellern Bilgrau <abilgrau
+#' @param x matrix containing expression values. May also be of class 
+#'   \code{ExpressionSet}. 
+#' @param center How should the data be centered. Can be either \code{"median"}, 
+#'   \code{"mean"}, or \code{"FALSE"}.
+#' @param scale How should the data be scaled. Can be either \code{"sd"} or 
+#'   \code{"FALSE"}.
+#' @return Expression matrix consisting of genewise median centred and scaled 
+#'   genes.
+#' @details First each probe-set is median centred. Next, each probe-set is 
+#'   scaled to have unit-variance.
+#' @references Reference to the \code{hemaClass.com} paper
+#' @author 
+#'   Steffen Falgreen <sfl (at) rn.dk> \cr
+#'   Anders Ellern Bilgrau <abilgrau
 #' (at) math.aau.dk>
 #' @examples
 #' 2+2
 #' @importFrom Biobase exprs "exprs<-"
 #' @importFrom matrixStats rowMedians
-#' 
 #' @export
-microarrayScale <- function(x, center = "median", scale = "sd"){
-  if(class(x) == "ExpressionSet"){
+microarrayScale <- function(x, center = "median", scale = "sd") {
+  if (class(x) == "ExpressionSet") {
     x.m <- Biobase::exprs(x)
-  }else{
+  } else {
     x.m <- x
   }
   
-  if(center == "median")
+  if(center == "median") {
     x.m <-  x.m - matrixStats::rowMedians(x.m,  na.rm = TRUE)
-  
-  if(center == "mean")
+  }
+  if(center == "mean") {
     x.m <-  x.m - rowMeans(x.m,  na.rm = TRUE)
-  
-  if(scale == "sd")
+  }
+  if (scale == "sd") {
     x.m <- x.m / rowSds(x.m, na.rm = TRUE) 
-  
-  if(class(x) == "ExpressionSet"){
+  }
+  if (class(x) == "ExpressionSet") {
     Biobase::exprs(x)  <- x.m
-  }else{
+  } else {
     x <- x.m
   }
-    
+  
   return(x)
 }
