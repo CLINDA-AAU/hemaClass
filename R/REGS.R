c6de1bf569edb032ef54481d545ebf01dfab6a5b
Anders E Bilgrau <anders.ellern.bilgrau@gmail.com>
2015-10-02 10:21:51 +0200

    Code clean-up.

--- R/REGS.R
+++ R/REGS.R
@@ -88,7 +88,7 @@ ResistanceClassifier <- function(new.data,
                                  cut = list(Cyclophosphamide = c(0.455, 0.67),
                                             Doxorubicin      = c(0.1,   0.86),
                                             Vincristine      = c(0.38,  0.54),
-                                            Combined         = c(0.067, 0.907))){
+                                            Combined         = c(0.067, 0.907))) {
   
   new.data[is.na(new.data)] <- 0
   
@@ -98,10 +98,10 @@ ResistanceClassifier <- function(new.data,
   
   train.mat <- train.mat[, wh, drop = FALSE]
   
-  if("Combined" %in% drugs & length(length(drugs) > 3)){
+  if ("Combined" %in% drugs & length(length(drugs) > 3)) {
     train.mat <- cbind(train.mat, apply(train.mat, 1, prod) / 
                          (apply(train.mat, 1, prod) +
-                            apply(1-train.mat, 1, prod)))
+                            apply(1 - train.mat, 1, prod)))
     colnames(train.mat) <- drugs
   }
   
@@ -118,28 +118,26 @@ ResistanceClassifier <- function(new.data,
   class <- as.data.frame(class)
   
   
-  for(i in 1:ncol(train.mat))
-    class[, i] <-
-    factor(class[, i], 
-           levels= c("Sensitive","Intermediate", "Resistant"))
+  for (i in 1:ncol(train.mat)) {
+    class[, i] <- factor(class[, i], 
+                         levels = c("Sensitive", "Intermediate", "Resistant"))
+  }
   
-  return(list(class = class, 
-              prob  = train.mat,
-              cut   = cut))
+  return(list(class = class, prob  = train.mat, cut   = cut))
 }
 
 
 #' @rdname REGS
 #' @export
 ResistancePredictor <- function(new.data, 
-                                 drugs = c("Cyclophosphamide", 
-                                           "Doxorubicin",
-                                           "Vincristine",
-                                           "Combined"),
-                                 cut = list(Cyclophosphamide = c(280, 340),
-                                            Doxorubicin      = c(280, 320),
-                                            Vincristine      = c(115,  127),
-                                            Combined         = c(200, 295))){
+                                drugs = c("Cyclophosphamide", 
+                                          "Doxorubicin",
+                                          "Vincristine",
+                                          "Combined"),
+                                cut = list(Cyclophosphamide = c(280, 340),
+                                           Doxorubicin      = c(280, 320),
+                                           Vincristine      = c(115,  127),
+                                           Combined         = c(200, 295))){
   
   new.data[is.na(new.data)] <- 0
   
@@ -149,10 +147,10 @@ ResistancePredictor <- function(new.data,
   
   train.mat <- train.mat[, wh, drop = FALSE]
   
-  if("Combined" %in% drugs & length(length(drugs) > 3)){
+  if ("Combined" %in% drugs & length(length(drugs) > 3)) {
     train.mat <- cbind(train.mat, apply(train.mat, 1, prod) / 
                          (apply(train.mat, 1, prod) +
-                            apply(1-train.mat, 1, prod)))
+                            apply(1 - train.mat, 1, prod)))
     colnames(train.mat) <- drugs
   }
   
@@ -161,20 +159,19 @@ ResistancePredictor <- function(new.data,
   class <- train.mat
   class[,] <- "Intermediate"
   
-  # Ceck that these are the correct way
-  class[train.mat < matrix(unlist(rep(data.frame(cut)[1,], each = nrow(train.mat)) ),
+  # Check that these are the correct way
+  class[train.mat < matrix(unlist(rep(data.frame(cut)[1,], each = nrow(train.mat))),
                            nrow = nrow(train.mat), byrow = FALSE)] <- "Sensitive"
-  class[train.mat > matrix(unlist(rep(data.frame(cut)[2,], each = nrow(train.mat)) ),
+  class[train.mat > matrix(unlist(rep(data.frame(cut)[2,], each = nrow(train.mat))),
                            nrow = nrow(train.mat), byrow = FALSE)] <- "Resistant"
   
   class <- as.data.frame(class)
   
   
-  for(i in 1:ncol(train.mat))
-    class[, i] <-
-    factor(class[, i], 
-           levels= c("Sensitive","Intermediate", "Resistant"))
-  
+  for (i in 1:ncol(train.mat)) {
+    class[, i] <- factor(class[, i], 
+                         levels = c("Sensitive", "Intermediate", "Resistant"))
+  }
   return(list(class = class, 
               pred  = train.mat,
               cut   = cut))
@@ -183,300 +180,293 @@ ResistancePredictor <- function(new.data,
 
 #' @rdname REGS
 #' @export
-CyclophosphamideClassifier <-
-  function(new.data){
-    
-    coef <- readCyclophosphamideClasCoef()
-    
-    x <- rbind(1, new.data[names(coef)[-1],,drop= FALSE])
-    
-    prob <- t(x) %*% as.matrix((coef)  )
-    
-    prob <- 1 - exp(prob) / ( exp(prob) + exp(-prob))
-    colnames(prob) <- "Sensitive"
-    prob
-  }
+CyclophosphamideClassifier <- function(new.data) {
+  coef <- readCyclophosphamideClasCoef()
+  
+  x <- rbind(1, new.data[names(coef)[-1], , drop = FALSE])
+  
+  prob <- t(x) %*% as.matrix((coef))
+  
+  prob <- 1 - exp(prob) / (exp(prob) + exp(-prob))
+  colnames(prob) <- "Sensitive"
+  return(prob)
+}
 
 #' @rdname REGS
 #' @export
-CyclophosphamidePredictor <- 
-  function(new.data){
-    coef <- readCyclophosphamidePredCoef()
-    
-    x <- rbind(1, new.data[names(coef)[-1],,drop= FALSE])
-    
-    t(x) %*% as.matrix((coef))
-  }
+CyclophosphamidePredictor <- function(new.data) {
+  coef <- readCyclophosphamidePredCoef()
+  
+  x <- rbind(1, new.data[names(coef)[-1], , drop = FALSE])
+  
+  return(t(x) %*% as.matrix(coef))
+}
 
 
 #' @rdname REGS
 #' @export
-DoxorubicinClassifier <-
-  function(new.data){
-    
-    coef <- readDoxorubicinClasCoef()
-    
-    x <- rbind(1, new.data[names(coef)[-1],,drop= FALSE])
-    
-    prob <- t(x) %*% as.matrix((coef)  )
-    
-    prob <- 1 - exp(prob) / ( exp(prob) + exp(-prob))
-    colnames(prob) <- "Sensitive"
-    prob
-  }
+DoxorubicinClassifier <- function(new.data) {
+  coef <- readDoxorubicinClasCoef()
+  
+  x <- rbind(1, new.data[names(coef)[-1], , drop = FALSE])
+  
+  prob <- t(x) %*% as.matrix((coef))
+  
+  prob <- 1 - exp(prob) / (exp(prob) + exp(-prob))
+  colnames(prob) <- "Sensitive"
+  return(prob)
+}
 
 #' @rdname REGS
 #' @export
-DoxorubicinPredictor <- 
-  function(new.data){
-    coef <- readDoxorubicinPredCoef()
-    
-    x <- rbind(1, new.data[names(coef)[-1],,drop= FALSE])
-    
-    t(x) %*% as.matrix((coef))
-  }
+DoxorubicinPredictor <- function(new.data) {
+  coef <- readDoxorubicinPredCoef()
+  
+  x <- rbind(1, new.data[names(coef)[-1], , drop = FALSE])
+  
+  return(t(x) %*% as.matrix(coef))
+}
 
 
 #' @rdname REGS
 #' @export
-VincristineClassifier <-
-  function(new.data){
-    
-    coef <- readVincristineClasCoef()
-    
-    x <- rbind(1, new.data[names(coef)[-1],,drop= FALSE])
-    
-    prob <- t(x) %*% as.matrix((coef)  )
-    
-    prob <- 1 - exp(prob) / ( exp(prob) + exp(-prob))
-    colnames(prob) <- "Sensitive"
-    prob
-  }
+VincristineClassifier <- function(new.data) {
+  coef <- readVincristineClasCoef()
+  
+  x <- rbind(1, new.data[names(coef)[-1], , drop = FALSE])
+  
+  prob <- t(x) %*% as.matrix((coef)  )
+  
+  prob <- 1 - exp(prob) / (exp(prob) + exp(-prob))
+  colnames(prob) <- "Sensitive"
+  return(prob)
+}
 
 #' @rdname REGS
 #' @export
-VincristinePredictor <- 
-  function(new.data){
-    coef <- readVincristinePredCoef()
-    
-    x <- rbind(1, new.data[names(coef)[-1],,drop= FALSE])
-    
-    t(x) %*% as.matrix((coef))
-  }
+VincristinePredictor <- function(new.data) {
+  coef <- readVincristinePredCoef()
+  
+  x <- rbind(1, new.data[names(coef)[-1], , drop = FALSE])
+  
+  return(t(x) %*% as.matrix((coef)))
+}
 
-ResistancePredFun <- function(newx, drugs = c("Cyclophosphamide", "Doxorubicin", "Vincristine")){
+ResistancePredFun <- function(newx, 
+                              drugs = c("Cyclophosphamide", "Doxorubicin", 
+                                        "Vincristine")) {
   
   coef <- readPredCoef()
   
   diff <- setdiff(row.names(coef)[-1], rownames(newx))
   
-  if(length(diff)){
+  if (length(diff)) {
     missing <-  matrix(0, ncol = ncol(newx), nrow = length(diff), 
                        dimnames = list(diff, colnames(newx) ))
     
     newx <- rbind(newx, missing)
     
-    warning("the following probesets are missing:\n", paste(diff, collapse = ", "))
+    warning("the following probesets are missing:\n", 
+            paste(diff, collapse = ", "))
   }
   
-  x <- rbind(1, newx[row.names(coef)[-1],,drop= FALSE])
+  x <- rbind(1, newx[row.names(coef)[-1], , drop = FALSE])
   
-  t(x) %*% coef[, drugs, drop= FALSE]
+  return(t(x) %*% coef[, drugs, drop = FALSE])
 }
 
 
-ResistanceProbFun <- function(newx, drugs = c("Cyclophosphamide", "Doxorubicin", "Vincristine")){
+ResistanceProbFun <- function(newx, 
+                              drugs = c("Cyclophosphamide", "Doxorubicin", 
+                                        "Vincristine")) {
   
   coef <- readClasCoef()
   
   diff <- setdiff(row.names(coef)[-1], rownames(newx))
   
-  if(length(diff)){
+  if (length(diff)) {
     missing <-  matrix(0, ncol = ncol(newx), nrow = length(diff), 
                        dimnames = list(diff, colnames(newx) ))
     
     newx <- rbind(newx, missing)
     
-    warning("the following probesets are missing:\n", paste(diff, collapse = ", "))
+    warning("the following probesets are missing:\n", 
+            paste(diff, collapse = ", "))
   }
-  x <- rbind(1, newx[row.names(coef)[-1],,drop= FALSE])
+  x <- rbind(1, newx[row.names(coef)[-1], , drop = FALSE])
   
-  prob.mat <- t(x) %*% coef[, drugs, drop= FALSE]
+  prob.mat <- t(x) %*% coef[, drugs, drop = FALSE]
   
-  exp(-prob.mat) / ( exp(prob.mat) + exp(-prob.mat))
+  return(exp(-prob.mat) / (exp(prob.mat) + exp(-prob.mat)))
 }
 
 
 
 #' @rdname REGS
 #' @export
-RituximabClassifier <-
-  function(new.data, type = "corrected", cut = c(0.33, 0.66), 
-           calc.cut = NULL,
-           cut.spec = NULL, percent.classified = 85){
-    
-    if(grepl("lysis", type)){
-      
-      new.data[is.na(new.data)] <- 0
-      
-      train.mat <- RituximabProbFun(new.data, type = type)
-      
-      if(nrow(new.data) > 1){
-        class <- colnames(train.mat)[apply(train.mat, 1, which.max)]
-      }else{
-        class <- names(which.max(train.mat))
-      }
-      
-      if(nrow(new.data) > 1){
-        prob = apply(train.mat, 1, max)
-        if(is.null(cut.spec))
-          cut <- quantile(prob, 1-percent.classified / 100)    
-      }else{
-        cut <- 0
-        prob = max(train.mat)
-      }
-      
-      if(!is.null(cut.spec)) cut <- cut.spec
-      
-      class.pred <- class
-      
-      class <- factor(ifelse(prob < cut, "Unclassified", class),
-                      levels = c("Lytisk", "Statisk", "Resistant", "Unclassified"))
-      class <- as.matrix(class)
-      colnames(class) <- "Sensitivity"
-      return(list(class = class, 
-                  prob  = train.mat,
-                  cut   = cut)
-      )
-        
-    }else{
-      
-      if(type == "uncorrected")
-        coef <- readRituximabClasCoef()
-      if(type == "corrected")
-        coef <- readRituximabClasCorCoef()
-      
-      x <- rbind(1, new.data[names(coef)[-1],,drop= FALSE])
-      
-      prob <- t(x) %*% as.matrix((coef))
-      
-      prob <- exp(prob) / ( exp(prob) + exp(-prob))
-      colnames(prob) <- "Resistant"
-      
-      prob <- 1-prob # to obtain probability of being sensitive
-      colnames(prob) <- "Sensitivity"
-      
-      if(!is.null(calc.cut))
-        cut <- quantile(prob, calc.cut)
-      
-      class <- prob
-      class[] <- "Intermediate"
-      class[prob < min(cut)] <- "Resistant"
-      class[prob > max(cut)] <- "Sensitive"
-    
-      class <- as.data.frame(class)
+RituximabClassifier <- function(new.data,
+                                type = "corrected",
+                                cut = c(0.33, 0.66), 
+                                calc.cut = NULL,
+                                cut.spec = NULL, 
+                                percent.classified = 85){
   
-      class[,1] <-
-        factor(class[,1], 
-               levels= c("Sensitive","Intermediate", "Resistant"))
-      
-      return(list(class = class, 
-                  prob  = prob,
-                  cut   = cut))
+  if (grepl("lysis", type)) {
+    
+    new.data[is.na(new.data)] <- 0
+    
+    train.mat <- RituximabProbFun(new.data, type = type)
+    
+    if (nrow(new.data) > 1) {
+      class <- colnames(train.mat)[apply(train.mat, 1, which.max)]
+    } else {
+      class <- names(which.max(train.mat))
     }
-  }
-
-#' @rdname REGS
-#' @export
-RituximabPredictor <- 
-  function(new.data, type = "corrected", 
-           cut = c(0.33, 0.66), calc.cut = NULL){
-   
-    if(type == "uncorrected")
-      coef <- readRituximabPredCoef()
-    if(type == "corrected")
-      coef <- readRituximabPredCorCoef()
     
-    x <- rbind(1, new.data[names(coef)[-1],,drop= FALSE])
+    if (nrow(new.data) > 1) {
+      prob = apply(train.mat, 1, max)
+      if (is.null(cut.spec)) {
+        cut <- quantile(prob, 1 - percent.classified / 100)    
+      }
+    } else {
+      cut <- 0
+      prob = max(train.mat)
+    }
     
-    prob <- t(x) %*% as.matrix((coef))
+    if (!is.null(cut.spec)) {
+      cut <- cut.spec
+    }
     
-    # calculate classes
-    class <- prob
+    class <- 
+      factor(ifelse(prob < cut, "Unclassified", class),
+             levels = c("Lytisk", "Statisk", "Resistant", "Unclassified"))
+    class <- as.matrix(class)
+    colnames(class) <- "Sensitivity"
     
-    if(!is.null(calc.cut))
+    return(list(class = class, prob = train.mat, cut = cut))
+    
+  } else {
+    
+    if (type == "uncorrected") {
+      coef <- readRituximabClasCoef()
+    }
+    if (type == "corrected") {
+      coef <- readRituximabClasCorCoef()
+    }
+    
+    x <- rbind(1, new.data[names(coef)[-1], , drop = FALSE])
+    
+    prob <- t(x) %*% as.matrix(coef)
+    
+    prob <- exp(prob) / (exp(prob) + exp(-prob))
+    colnames(prob) <- "Resistant"
+    
+    prob <- 1 - prob # to obtain probability of being sensitive
+    colnames(prob) <- "Sensitivity"
+    
+    if (!is.null(calc.cut)) {
       cut <- quantile(prob, calc.cut)
-    
+    }
+    class <- prob
     class[] <- "Intermediate"
-    
-    if(type == "uncorrected"){
-      class[prob < min(cut)] <- "Sensitive"
-      class[prob > max(cut)] <- "Resistant"
-    }
-    
-    if(type == "corrected"){
-      class[prob < min(cut)] <- "Resistant"
-      class[prob > max(cut)] <- "Sensitive"
-    }
+    class[prob < min(cut)] <- "Resistant"
+    class[prob > max(cut)] <- "Sensitive"
     
     class <- as.data.frame(class)
     
-    class[,1] <-
-      factor(class[,1], 
-             levels= c("Sensitive","Intermediate", "Resistant"))
+    class[,1] <- factor(class[,1], 
+                        levels = c("Sensitive", "Intermediate", "Resistant"))
     
-    return(list(class = class, 
-                pred  = prob,
-                cut   = cut))
-    
+    return(list(class = class, prob = prob, cut = cut))
   }
+}
 
-
-RituximabProbFun <- function(newx, type = "lysis"){
- 
-  if(type == "lysis")
-    coef <- readRituximabClasLytiskCoef()
-  if(type == "lysis2")
-    coef <- readRituximabClasLytisk2Coef()
+#' @rdname REGS
+#' @export
+RituximabPredictor <- function(new.data, type = "corrected", 
+                               cut = c(0.33, 0.66), calc.cut = NULL) {
   
-  x <- rbind(1, newx[row.names(coef)[-1],,drop= FALSE])
+  if (type == "uncorrected") {
+    coef <- readRituximabPredCoef()
+  } else if (type == "corrected") {
+    coef <- readRituximabPredCorCoef()
+  }
+  x <- rbind(1, new.data[names(coef)[-1], , drop = FALSE])
+  
+  prob <- t(x) %*% as.matrix((coef))
+  
+  # calculate classes
+  class <- prob
+  
+  if (!is.null(calc.cut)) {
+    cut <- quantile(prob, calc.cut)
+  }
+  class[] <- "Intermediate"
+  
+  if (type == "uncorrected") {
+    class[prob < min(cut)] <- "Sensitive"
+    class[prob > max(cut)] <- "Resistant"
+  }
+  
+  if (type == "corrected") {
+    class[prob < min(cut)] <- "Resistant"
+    class[prob > max(cut)] <- "Sensitive"
+  }
+  
+  class <- as.data.frame(class)
+  
+  class[,1] <- factor(class[,1], 
+                      levels = c("Sensitive", "Intermediate", "Resistant"))
+  
+  return(list(class = class, 
+              pred  = prob,
+              cut   = cut))
+  
+}
+
+
+RituximabProbFun <- function(newx, type = "lysis") {
+  if (type == "lysis") {
+    coef <- readRituximabClasLytiskCoef()
+  } else if (type == "lysis2") {
+    coef <- readRituximabClasLytisk2Coef()
+  }
+  
+  x <- rbind(1, newx[row.names(coef)[-1], , drop = FALSE])
   
   prob.mat <- matrix(ncol = 3, nrow = ncol(x))
   
   colnames(prob.mat) <- c("Lytisk", "Statisk", "Resistant")
   
   rownames(prob.mat) <- colnames(x)
-  prob.mat[,1] <- t(x)%*%(as.matrix(coef)[,"Lytisk"]) 
-  prob.mat[,2] <- t(x)%*%(as.matrix(coef)[,"Statisk"]) 
-  prob.mat[,3] <- t(x)%*%(as.matrix(coef)[,"Resistant"]) 
+  prob.mat[,1] <- t(x) %*% as.matrix(coef)[, "Lytisk"] 
+  prob.mat[,2] <- t(x) %*% as.matrix(coef)[, "Statisk"]
+  prob.mat[,3] <- t(x) %*% as.matrix(coef)[, "Resistant"]
   
   prob.mat <- exp(prob.mat)
-  prob.mat / rowSums(prob.mat)
+  return(prob.mat / rowSums(prob.mat))
 }
 
 
 
 #' @rdname REGS
 #' @export
-DexamethasoneClassifier <-
-  function(new.data){
-    
-    coef <- readDexamethasoneClasCoef()
-    
-    x <- rbind(1, new.data[names(coef)[-1],,drop= FALSE])
-    
-    prob <- t(x) %*% as.matrix((coef)  )
-    
-    prob <- 1 - exp(prob) / ( exp(prob) + exp(-prob))
-    colnames(prob) <- "Sensitive"
-    prob
-  }
+DexamethasoneClassifier <- function(new.data){
+  coef <- readDexamethasoneClasCoef()
+  
+  x <- rbind(1, new.data[names(coef)[-1], , drop = FALSE])
+  
+  prob <- t(x) %*% as.matrix(coef)
+  prob <- 1 - exp(prob)/(exp(prob) + exp(-prob))
+  colnames(prob) <- "Sensitive"
+  return(prob)
+}
 
 #' @rdname REGS
 #' @export
 DexamethasonePredictor <- function(new.data) {
   coef <- readDexamethasonePredCoef()
-
+  
   x <- rbind(1, new.data[names(coef)[-1], , drop = FALSE])
   
   return(t(x) %*% as.matrix((coef)))
