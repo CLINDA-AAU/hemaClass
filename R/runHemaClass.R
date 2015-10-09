f2d2a0b0081b1d203605cab5494ca6ec8993e70c
Anders E Bilgrau <anders.ellern.bilgrau@gmail.com>
2015-10-09 11:01:42 +0200

    Added some features to runHemaClass. Expanded documentation. Now opens, by default directly in browser. Allowed for "showcase/debug" modde.

--- R/runHemaClass.R
+++ R/runHemaClass.R
@@ -1,14 +1,26 @@
 #' Run the shiny server for hemaClass.
 #' 
-#' Run a local version of the server running on \code{hemaclass.com}. Provides
-#' an easy web-based interface for the \pkg{hemaClass} package.
+#' Run a local version of the server running on \url{http://hemaclass.org}. 
+#' Provides an easy-to-used and web-based interface for the \pkg{hemaClass} 
+#' package.
 #' 
-#' @aliases 
-#'   runHemaClass
-#' @return Starts a shiny server for the analysis
+#' @aliases runHemaClass runhemaclass
+#' @param debug.help logical. Should the code be displayed next to or beneath
+#'   the web application. Useful when debugging or showcasing the app.
+#' @param launch.browser logical. If \code{TRUE}, the web browser is used.
+#'   Default is \code{TRUE}. If \code{FALSE}, the default from 
+#'   \code{\link{runApp}} is used.
+#' @param \dots Arguments passed to \code{\link{runApp}}.
+#' @return Starts up a local Shiny server instance of the 
+#'   \url{http://hemaclass.org} website for faster analysis.
+#' @note If the Rstudio viewer is used, only a single \code{.CEL} file can be 
+#'   uploaded, please use your browser instead.
+#' @references \url{http://hemaclass.org}
+#' @seealso \code{\link{runApp}}
 #' @author 
-#'   Steffen Falgreen Larsen \cr
-#'   Anders Ellern Bilgrau
+#'   Steffen Falgreen Larsen <falgreen (at) me.com>\cr
+#'   Anders Ellern Bilgrau \cr
+#'   Jonas Have
 #' @export
 #' @import tools gdata WriteXLS
 #' @importFrom shinysky busyIndicator
@@ -16,11 +28,19 @@
 #' @importFrom RLumShiny jscolorInput
 #' @examples
 #' \dontrun{
-#' library("gdata")
-#' library("survival")
-#' library("shinysky")
 #' runHemaClass()
+#' 
+#' # For a showcase/debug help mode:
+#' runHemaClass(debug.help = TRUE)
+#' 
+#' # To use the Rstudio viewer:
+#' runHemaClass(launch.browser = FALSE) 
 #' }
-runHemaClass <- function() {
-  shiny::runApp(system.file("website/", package = 'hemaClass'))
+runHemaClass <- function(launch.browser = TRUE, debug.help = FALSE, ...) {
+  if (!isTRUE(launch.browser)) {
+    launch.browser <- getOption("shiny.launch.browser", interactive())
+  }
+  shiny::runApp(system.file("website/", package = 'hemaClass'),
+                launch.browser = launch.browser,
+                display.mode = ifelse(debug.help, "showcase", "auto"))
 }
