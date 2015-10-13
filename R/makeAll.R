7d34026dcef13a77a95c10dc91ff980ca5b08dbc
Anders E Bilgrau <anders.ellern.bilgrau@gmail.com>
2015-10-13 15:34:22 +0200

    Added makeHowto() to automatically generate the Howto.html for the website. Minor tweaks and adjustmets to the familiy of makeAll(). Tweaked documentation.

--- R/makeAll.R
+++ R/makeAll.R
@@ -1,12 +1,17 @@
 #' Generate news, celfiles, and help for website
 #' 
-#' Functions for generating the \code{News.html}, \code{celfiles.zip}, and
-#' \code{howto.html} files for the website from \code{NEWS.Rd}, the celfiles in 
-#' \code{"/"}, an \code{vignettes/howto.Rmd}. Intented mostly for internal use.
+#' Utility functions for generating the \code{News.html}, \code{celfiles.zip}, 
+#' and \code{howto.html} files used in the website from \code{NEWS.Rd}, 
+#' the celfiles in \code{"inst/extdata/celfiles"}, and
+#' \code{vignettes/howto.Rmd}, respectively. 
+#' Intented primarily for internal use.
 #' 
+#' @param makeCelfiles logical. Should \code{makeCelfiles()} be executed.
+#'   Default \code{FALSE} is faster.
 #' @param indir, outdir The in and output directories to read and write the 
 #'   files from and to.
 #' @param overwrite Overwrite existing files.
+#' @param \dots Arguments passed to \code{knitr:knit2html}.
 #' @return Writes to files in \code{outdir}. Invisibly returns the path to the 
 #'   written file.
 #' @note The functions assumes by default that the working directory is the
@@ -19,13 +24,13 @@
 #' 
 #' # makeAll is an alias for:
 #' makeNews()
-#' #makeCelfiles() 
+#' makeCelfiles() 
 #' makeHowto()
 #' }
 #' @export
-makeAll <- function() {
+makeAll <- function(makeCelfiles = FALSE) {
   makeNews()
-  # makeCelfiles()
+  if (makeCelfiles) makeCelfiles()
   makeHowto()
 }
   
@@ -46,13 +51,6 @@ makeNews <- function(indir = "inst", outdir = "inst/website/www") {
   html <- gsub('Package <span class="pkg">hemaClass</span>', 
                new.title, html)
   
-  # Add header
-  html <- c(paste("<!-- Generated using hemaClass::makeNews()",
-                  "from inst/News.Rd", Sys.Date(), "-->"),
-            "<!-- Do not edit by hand! -->",
-            "<!-- Edit inst/News.Rd -->",
-            html)
-  
   # Add explanation text
   extra.text <- "Below are the latest news and changes for the
     <strong>hemaClass</strong> R-package."
@@ -64,6 +62,13 @@ makeNews <- function(indir = "inst", outdir = "inst/website/www") {
   html <- gsub("</h2>", "</h1>", html)
   html <- gsub("<h3>", "<h2>", html)
   html <- gsub("</h3>", "</h2>", html)
+  
+  # Prepend header
+  html <- c(paste("<!-- Generated using hemaClass::makeNews()",
+                  "from", infile, "on", Sys.Date(), "-->"),
+            "<!-- Do not edit by hand! -->",
+            paste("<!-- Edit",  infile, "instead. -->"),
+            html)
   
   # Write changes
   writeLines(html, con = outfile) 
@@ -83,6 +88,54 @@ makeCelfiles <- function(outdir = "inst/website/www") {
 
 #' @export
 #' @rdname makeAll
-makeHowto <- function(indir = "vignettes", outdir = "inst/website/www") {
+makeHowto <- function(indir = "vignettes", outdir = "inst/website/www", ...) {
   
+  infile <- file.path(indir, "Howto.Rmd")
+  if (!file.exists(infile)) stop("Cannot find file ", infile)
+  outfile <- file.path(outdir, "howto.html")
+  if (!file.exists(outfile)) stop("Cannot find file ", outfile)
+    
+  knitr::knit2html(input = infile, output = outfile, ...)  # Write to disk
+  
+  # Extra modifications to file
+  # Comment out header sections (remove formatting for website):
+  getind <- function(regex) min(which(grepl(regex, html, fixed = TRUE)))
+  beginCommentAfter <- function(regex) {
+    append(html, "<!-- ", after = min(getind(regex)))
+  }
+#   endCommentAfter <- function(regex) {
+#     append(html, " -->", after = min(getind(regex)))
+#   }
+  beginCommentBefore <- function(regex) {
+    append(html, "<!-- ", after = min(getind(regex)) - 1)
+  }
+  endCommentBefore <- function(regex) {
+    append(html, " -->", after = min(getind(regex)) - 1)
+  }
+  
+  html <- readLines(outfile)
+  html <- beginCommentBefore("<script type=\"text/javascript\">")
+  html <- endCommentBefore("<!-- Styles for R syntax highlighter -->")
+  html <- beginCommentAfter("<!-- Styles for R syntax highlighter -->")
+  html <- endCommentBefore("<!-- R syntax highlighter -->") 
+  html <- beginCommentAfter("<!-- R syntax highlighter -->")
+  html <- endCommentBefore("</head>")
+  
+  # Fix paths
+  html <- gsub("../inst/website/www/", "", html)
+  
+  # Prepend header
+  html <- c(paste("<!-- Generated using hemaClass::makeHowto()",
+                  "from", infile, "on", Sys.Date(), "-->"),
+            "<!-- Do not edit by hand! -->",
+            paste("<!-- Edit",  infile, "instead. -->"),
+            html)
+  
+  # Write to disk
+  writeLines(html, con = outfile) 
+  
+  # Remove generated .md file in getwd()
+  file.remove(gsub("\\.Rmd", ".md", basename(infile)))
+ 
+  return(invisible(outfile)) 
 }
